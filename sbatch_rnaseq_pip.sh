80f4e79cd1ef260b72c033d700f9b8d3a8f7cc81
maria13 <mariakondili13@gmail.com>
2021-04-21 11:40:21 +0200

    renew command after nextflow command changes

--- sbatch_rnaseq_pip.sh
+++ sbatch_rnaseq_pip.sh
@@ -7,8 +7,12 @@
 
 
 module load nextflow/20.04.1
-
+cd /home/MBNL_DCT/
+mkdir -p rnaseq_pip
 # run : -c <config_file> run -w <workspace>  <nextflow_pip_script.nf>
-nextflow -c /home//MBNL_DCT/config_pip.conf  \
-            run -w /mouse_seq/NXF_STAR/PIP_out \
-            /mouse_analysis/rnaseq_nxf_pip.nf
+# Changed in 2021: nextflow run -w nxf_workspace rnaseq_pip/
+#..where rnaseq_pip/ contains : nextflow.config , main.nf, and fastq_samplesheet.tsv
+# nxf_workspace : directory will be created by Nextflow, and keep tmp files of run.
+# attention: main.nf and nextflow.config should always be named this way.
+
+nextflow run -w nxf_workspace rnaseq_pip
