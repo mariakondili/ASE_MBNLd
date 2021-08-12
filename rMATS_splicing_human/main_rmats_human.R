96e596e60d60ae065b9030e85687cec1eb8a4042
maria13 <mariakondili13@gmail.com>
2021-08-12 15:12:33 +0200

    call mean_psi-and-correction-function in main
    delete donut_piechart script, keep function with full_corr=80 in input, call it from main

--- rMATS_splicing_human/main_rmats_human.R
+++ rMATS_splicing_human/main_rmats_human.R
@@ -4,6 +4,10 @@
 ###-- Author  : Maria Kondili
 ###-- Date    : 11 March 2021
 
+
+library(dplyr)
+library(ggplot2)
+
 work_dir="/projects/mbnl_dct/human_cellline/rMATS_SplicingCounts/tasks_Prep_Post_Stat/"
 post_dir <- paste0(work_dir,"Post_Ctrl_vs_DM1_vs_MBNLdecoy/")
 stats_pairs <-  c(paste0(post_dir,"Ctrl_vs_DM1"),
@@ -72,3 +76,46 @@ title="CTRL_vs_DM1_vs_MBNLdecoy"
 plot_name="Pheatmap_SignifAltSplicing_dPSI=0.20_Ctrl-DM1-MBNLdecoy.png"
 
 plot_Pheatmap(ase, signif.co, psi.co, title, conditions=c("CTRL","DM1","MBNLd"), plot_name)
+
+
+####---Calc MEAN PSI per Condition ----####
+
+ctrl  <- grep("IncLevel1_*", colnames(all_ase))
+dm1   <- grep("IncLevel2_*", colnames(all_ase))
+mbnld <- grep("IncLevel3_*", colnames(all_ase))
+
+mean_ctrl  <- rowMeans(all_ase[,ctrl])
+mean_dm1   <- rowMeans(all_ase[, dm1])
+mean_mbnld <- rowMeans(all_ase[,mbnld])
+
+all_ase$Mean_Psi_CTRL <- mean_ctrl
+all_ase$Mean_Psi_DM1  <- mean_dm1
+all_ase$Mean_Psi_MBNLd <- mean_mbnld
+
+
+##---- Correction-----##  =  ((MNBLd - DM1 ) / (CTRL - DM1) )*100
+
+all_ase$correction_Pcnt <- with(all_ase,
+                                 ((Mean_Psi_MBNLd - Mean_Psi_DM1)/ (Mean_Psi_CTRL -Mean_Psi_DM1)) *100)
+
+ ## OR:
+#all_ase$correction_Pcnt_v2 <-  with(all_ase, (1- (dPSI_Ctrl_vs_MBNLd / dPSI_Ctrl_vs_DM1)) *100)
+# is.na =  27499
+
+all_ase$correction_Pcnt <- round(all_ase$correction_Pcnt,2)
+
+post_dir <- "Post_CTRL_vs_DM1_vs_MBNLd/"
+outdir.ase <- paste0(post_dir,"Final_Results/")
+write.table(all_ase[order(all_ase$FDR_Ctrl_vs_MBNLd, decreasing = F),],
+            paste0(outdir.ase,"All_ASE_Ctrl_DM1_MBNLd_FDRsorted+Correction.tsv"),
+            sep="\t",col.names = T,quote=F)
+
+# call function:
+source("piechart_correction_rmats.R")
+
+## 1st Filter BY | dPSI | > 0.15 or |dPSI| > 0.20 :
+#my_psi_co= 0.15
+my_psi_co=0.20
+
+piechart_for_correction(all_ase, psi.co=my_psi_co, fdr.co=0.05,full_corr=80,
+                        plot_name=paste0("PieChart_Correction_filt_ASE_CTRL-DM1-MBNLd_dPSI=",my_psi_co,".png") )
